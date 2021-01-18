185a9ff383029fca535c660c865a460af1fbe8f4
tchawou-daniel <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-01-18 18:59:27 +0100

    :wrench: improve config.js

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,6 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://tobedefined.io/api"
-    : "https://mysterious-anchorage-24698.herokuapp.com/";
+    ? "https://mysterious-anchorage-24698.herokuapp.com/api"
+    : "http://localhost:2020/api";
+    
 export default API_URL;
